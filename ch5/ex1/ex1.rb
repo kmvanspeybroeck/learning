8d967bdd85e105bf1cbb52a3544255e5ba74aa4c
kmvanspeybroeck <kmvanspeybroeck@containerstore.com>
2013-07-08 08:51:05 -0500

    Create ex1.rb

---
+++ ch5/ex1/ex1.rb
@@ -0,0 +1,8 @@
+puts 'Please enter your first name.'
+first_name = gets.chomp
+puts 'Now, please enter your middle name.'
+middle_name = gets.chomp
+puts 'Finally, what is your last name?'
+last_name = gets.chomp
+puts ''
+puts 'Hello there ' + first_name + ' ' + middle_name + ' ' + last_name + '.  What an awesome name.'
