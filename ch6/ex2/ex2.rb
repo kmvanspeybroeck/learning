46fedb7287163b9fb6323e6b1398b922e9b4a9c1
kmvanspeybroeck <kmvanspeybroeck@containerstore.com>
2013-07-16 14:06:43 -0500

    Ch6 Ex2

--- ch6/ex2/ex2.rb
+++ ch6/ex2/ex2.rb
@@ -8,5 +8,6 @@ chp3starts = 'page 13'
 puts 'Table of Contents'.center(line_width)
 puts
 puts chp1nam.ljust(line_width/2) + chp1starts.rjust(line_width/2 - 2)
+puts chp1nam.ljust(line_width/2) + chp1starts.rjust(line_width/2 - 2)
 puts chp2nam.ljust(line_width/2) + chp2starts.rjust(line_width/2)
 puts chp3nam.ljust(line_width/2) + chp3starts.rjust(line_width/2)
