a014470f784d413a893f957311165620488e6a3d
kmvanspeybroeck <kmvanspeybroeck@containerstore.com>
2013-07-16 14:00:23 -0500

    Ch6 Ex2

---
+++ ch6/ex2/ex2.rb
@@ -0,0 +1,12 @@
+line_width = 50
+chp1nam = 'Chapter 1:  Getting Started'
+chp2nam = 'Chapter 2:  Numbers'
+chp3nam = 'Chapter 3:  Letters'
+chp1starts = 'page  1'
+chp2starts = 'page  9'
+chp3starts = 'page 13'
+puts 'Table of Contents'.center(line_width)
+puts
+puts chp1nam.ljust(line_width/2) + chp1starts.rjust(line_width/2 - 2)
+puts chp2nam.ljust(line_width/2) + chp2starts.rjust(line_width/2)
+puts chp3nam.ljust(line_width/2) + chp3starts.rjust(line_width/2)
